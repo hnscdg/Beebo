46e4700f466cac2b461333def95b1e1a5add9021
winhome <winhome@winhome.com>
2018-09-10 14:44:45 +0800

    winhome-use gulp and shelljs to build main.js

--- browserify/gulpfile.js
+++ browserify/gulpfile.js
@@ -1,3 +1,6 @@
 var gulp = require('gulp');
+var shelljs = require('shelljs');
 
-gulp.task('default', function(){console.log("This is default gulp")});
+gulp.task('default', function(){
+    shelljs.exec('browserify index.js -o main.js');
+});
