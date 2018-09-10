9ad407b7ba5f1d5a4aefcf7046ecb5f88e883e16
winhome <winhome@winhome.com>
2018-09-10 14:51:34 +0800

    winhome-use browserify api through gulp build main.js

--- browserify/gulpfile.js
+++ browserify/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var shelljs = require('shelljs');
-
+var browserify = require('browserify');
+var fs = require('fs');
 gulp.task('default', function(){
-    shelljs.exec('browserify index.js -o main.js');
+    browserify().add('index.js').bundle().pipe(fs.createWriteStream('main.js'));
 });
